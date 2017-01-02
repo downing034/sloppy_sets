16d971083c9377144cb628c7824a28a8ab7a1d10
Paul Downing <downing034@gmail.com>
2017-01-02 02:52:07 -0600

    added image uplaod for player avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230190359) do
+ActiveRecord::Schema.define(version: 20170102081917) do
 
   create_table "calendars", force: :cascade do |t|
     t.string   "name"
@@ -34,9 +34,17 @@ ActiveRecord::Schema.define(version: 20161230190359) do
     t.string   "college"
     t.integer  "number"
     t.string   "favorite_beer"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "nickname"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "profile_image_file_name"
+    t.string   "profile_image_content_type"
+    t.integer  "profile_image_file_size"
+    t.datetime "profile_image_updated_at"
   end
 
 end
